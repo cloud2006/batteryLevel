784edaa92ad08447819c8ad99c832c5231d33323
Pavlo Antoniuk <cloud.2006.a@gmail.com>
2025-08-07 12:59:04 +0300

    Initial Commit

---
+++ TestBatteryMonitor/TestBatteryMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBatteryMonitorApp.swift
+//  TestBatteryMonitor
+//
+//  Created by Pavlo Antoniuk on 07.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestBatteryMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
